7a2fbe9683fd8f3125cbac90bd080bf295ac381a
rick-23 <swamynathan.radhakrishnan@gmail.com>
2023-12-16 19:30:19 -0500

    Frontend Scaffold from Vite

---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <main>
+      <div>
+        <p className="read-the-docs">Welcome to AI Chatbot</p>
+      </div>
+    </main>
+  );
+}
+
+export default App;
