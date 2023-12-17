f99290bf466eac45858c46b0eef5eedba4e6e2c3
rick-23 <swamynathan.radhakrishnan@gmail.com>
2023-12-16 19:21:01 -0500

    Chat api setup and ReadMe file updation

---
+++ backend/README.md
@@ -0,0 +1,3 @@
+This is the backend of the customized chatbot project built on MERN stack with OpenAI, where each message of the user is stored in DB and can be retrieved and deleted.
+
+It's a completely secure application using JWT Tokens, HTTP-Only Cookies, Signed Cookies, Password Encryption, and Middleware Chains.
--- backend/src/routes/chat-routes.ts
+++ backend/src/routes/chat-routes.ts
@@ -1,5 +1,21 @@
 import { Router } from "express";
+import { chatCompletionValidator, validate } from "../utils/validators";
+import { verifyToken } from "../utils/token-manager";
+import {
+  deleteChats,
+  generateChatCompletion,
+  sendChatsToUser,
+} from "../controllers/chat-controller";
 
 const chatRoutes = Router();
+
+chatRoutes.post(
+  "/new",
+  validate(chatCompletionValidator),
+  verifyToken,
+  generateChatCompletion
+);
+chatRoutes.get("/all-chats", verifyToken, sendChatsToUser);
+chatRoutes.delete("/delete", verifyToken, deleteChats);
 
 export default chatRoutes;
